aa995790d3a9e364c987ea8c2d03dfbc6bb086f6
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-09-01 14:55:20 +0900

    Adds package setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'satoyama',
+  packages = ['satoyama'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Datamodels and utilities',
+  author = 'Halfdan Rump',
+  author_email = 'dalekk@gmail.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/DgFutureLab/satoyama/tarball/0.1', # I'll explain this in a second
+  keywords = ['sensor network', 'models'], # arbitrary keywords
+  classifiers = [],
+)
