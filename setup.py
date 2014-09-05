4465e6decf46ddcc80ed8f917c5007073f638a37
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-09-05 14:50:05 +0900

    Changed version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'satoyama',
   packages = ['satoyama'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.1',
   description = 'Datamodels and utilities for sensor networks',
   author = 'Halfdan Rump',
   author_email = 'dalekk@gmail.com',
