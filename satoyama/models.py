7398707b98796fc69cd7f5c454dbcca9bf0b0823
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-10-07 16:00:22 +0900

    Fixes issue 3

--- satoyama/models.py
+++ satoyama/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.exc import IntegrityError, DataError
 from sqlalchemy.orm import relationship, object_mapper, class_mapper
 from collections import Iterable
 from helpers import DatetimeHelper
+import json
 
 def create(model):						### 'create' is the name of the decorator
 	@staticmethod
@@ -18,6 +19,8 @@ def create(model):						### 'create' is the name of the decorator
 			try:
 				db_session.add(instance)			### ..added to the session
 				db_session.commit()					### ..inserted to the database
+				if isinstance(instance, Reading):   ### If the new object is a Reading, add it as the lastest reading of the sensor
+					instance.sensor.latest_reading = json.dumps(instance.json('sensor', 'sensor_id'))				
 				return instance 					### ..and returned to the caller
 			except Exception, e:
 				db_session.rollback()
@@ -31,11 +34,16 @@ class SatoyamaBase():
 	def __init__(self):
 		self.messages = list()
 
-	def json(self, verbose = False):
+	def json(self, *exclude_fields):
 		jsondict = {}
 		for prop in object_mapper(self).iterate_properties: 
-			jsondict.update({prop.key: getattr(self, prop.key)})
+			if not prop.key in exclude_fields:
+				jsondict.update({prop.key: getattr(self, prop.key).__repr__()})
+			
+		jsondict.update({'type': str(type(self))})
 		return jsondict
+
+
 
 	@classmethod
 	def settables(cls):
@@ -66,8 +74,15 @@ class Node(SatoyamaBase, Base):
 		self.latitude = latitude
 		self.alias = alias
 
+
+
 	def __repr__(self):
-		return str({'id' : self.id})
+		json_dict = {'type' : str(self.__class__)}
+		if self.alias: 
+			json_dict.update({'alias' : self.alias})
+		else: 
+			json_dict.update({'id' : self.id})
+		return json.dumps(json_dict)
 
 @create
 class SensorType(SatoyamaBase, Base):
@@ -84,7 +99,12 @@ class SensorType(SatoyamaBase, Base):
 		self.unit = unit
 
 	def __repr__(self):
-		return str({'id' : self.id})
+		json_dict = {'type' : str(self.__class__)}
+		if self.name: 
+			json_dict.update({'name' : self.name})
+		else: 
+			json_dict.update({'id' : self.id})
+		return json.dumps(json_dict)
 
 @create
 class Sensor(SatoyamaBase, Base):
@@ -93,6 +113,7 @@ class Sensor(SatoyamaBase, Base):
 	id = Column( Integer, primary_key = True )
 	alias = Column( String() )
 	readings = relationship('Reading', backref = 'sensor')
+	latest_reading = Column( String() )
 	node_id = Column( Integer, ForeignKey('nodes.id') )
 	sensortype_id = Column( Integer, ForeignKey('sensortypes.id') )
 	
@@ -105,13 +126,19 @@ class Sensor(SatoyamaBase, Base):
 		self.sensortype = sensortype
 		self.node = node
 		self.alias = alias
+		self.latest_reading = None
 		
 		for reading in readings:
 			assert isinstance(reading, Reading), 'Each item in readings must be an instance of type Reading'
 			self.readings.append(reading)
 
 	def __repr__(self):
-		return str({'id' : self.id})
+		json_dict = {'type' : str(self.__class__), 'latest_reading' : self.latest_reading}
+		if self.alias: 
+			json_dict.update({'alias' : self.alias})
+		else: 
+			json_dict.update({'id' : self.id})
+		return json.dumps(json_dict)
 
 @create
 class Reading(SatoyamaBase, DatetimeHelper, Base):
@@ -125,7 +152,7 @@ class Reading(SatoyamaBase, DatetimeHelper, Base):
 	def __init__(self, sensor, value = None, timestamp = None):
 		super(Reading, self).__init__()
 		self.sensor = sensor
-		
+		# sensor.latest_reading = self	
 		if value:
 			try:
 				self.value = float(value)
@@ -134,7 +161,13 @@ class Reading(SatoyamaBase, DatetimeHelper, Base):
 				raise e
 
 		self.timestamp = self.convert_timestamp(timestamp)
+		print self.__repr__()
 	
 	def __repr__(self):
-		return str({'id' : self.id})
+		json_dict = {'type' : str(self.__class__)}
+		if self.value: 
+			json_dict.update({'value' : self.value})
+		else: 
+			json_dict.update({'id' : self.id})
+		return json.dumps(json_dict)
 
